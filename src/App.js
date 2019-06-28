37d5991f8eb0fa0885968bf5094fb5c54a697310
James J Williams JR <Jrotw@Jamess-MacBook-Pro.local>
2019-06-28 12:01:45 -0700

    Palette routing for each seed palette

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './ColorHelpers';
 
-function App (){
-	return (
-		<Switch>
-			<Route exact path='/' render={() => <h1>Palette</h1>} />
-			<Route exact path='/palette/:paletteId' render={() => <h1>Individual Palette</h1>} />
-		</Switch>
-		// <div>
-		// 	<Palette palette={generatePalette(seedColors[4])} />
-		// </div>
-	);
+class App extends Component {
+	findPalette (id) {
+		return seedColors.find(function (palette){
+			return palette.id === id;
+		});
+	}
+	render () {
+		return (
+			<Switch>
+				<Route exact path='/' render={() => <h1>Palette</h1>} />
+				<Route
+					exact
+					path='/palette/:paletteId'
+					render={(routeProps) => (
+						<Palette palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))} />
+					)}
+				/>
+			</Switch>
+		);
+	}
 }
 
 export default App;
