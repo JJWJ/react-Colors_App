4ac17617a9352812321dfd5bb472291e4d2fff7f
James J Williams JR <Jrotw@Jamess-MacBook-Pro.local>
2019-06-28 11:36:21 -0700

    React-router-dom setting up switch and route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './ColorHelpers';
 
 function App (){
 	return (
-		<div className='App'>
-			<Palette palette={generatePalette(seedColors[4])} />
-		</div>
+		<Switch>
+			<Route exact path='/' render={() => <h1>Palette</h1>} />
+			<Route exact path='/palette/:paletteId' render={() => <h1>Individual Palette</h1>} />
+		</Switch>
+		// <div>
+		// 	<Palette palette={generatePalette(seedColors[4])} />
+		// </div>
 	);
 }
 
