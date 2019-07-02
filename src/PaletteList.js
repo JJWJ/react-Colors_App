df5ca430f48cfa1dee011813f1c378c742ce8599
James J Williams JR <Jrotw@Jamess-MacBook-Pro.local>
2019-07-02 05:23:30 -0700

    Demo of withStyles from @material-ui/styles

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import MiniPalette from './MiniPalette';
 
 class PaletteList extends Component {
 	render () {
 		const { palette } = this.props;
 		return (
 			<div>
+				<MiniPalette />
 				<h1>Color Palette Creator</h1>
 				{palette.map((palette) => (
 					<p>
