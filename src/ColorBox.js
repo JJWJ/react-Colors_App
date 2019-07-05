223522eaced3d9d334ca6f8b5cdd271430656666
James J Williams JR <Jrotw@Jamess-MacBook-Pro.local>
2019-07-04 20:56:26 -0700

    Complete refactor of ColorBox CSS to JSS

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -6,7 +6,7 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { withStyles } from '@material-ui/styles';
 
 const styles = {
-	ColorBox   : {
+	ColorBox        : {
 		width                 : '20%',
 		height                : (props) =>
 
@@ -22,19 +22,19 @@ const styles = {
 			transition : '0.5s',
 		},
 	},
-	copyText   : {
+	copyText        : {
 		color : (props) =>
 
 				chroma(props.background).luminance() >= 0.08 ? 'rgba(0,0,0,0.7)' :
 				'white',
 	},
-	colorName  : {
+	colorName       : {
 		color : (props) =>
 
 				chroma(props.background).luminance() <= 0.08 ? 'white' :
 				'rgba(0,0,0,0.7)',
 	},
-	seeMore    : {
+	seeMore         : {
 		color         : (props) =>
 
 				chroma(props.background).luminance() >= 0.08 ? 'rgba(0,0,0,0.7)' :
@@ -49,7 +49,7 @@ const styles = {
 		lineHeight    : '2rem',
 		textTransform : 'uppercase',
 	},
-	copyButton : {
+	copyButton      : {
 		color          : (props) =>
 
 				chroma(props.background).luminance() >= 0.08 ? 'rgba(0,0,0,0.7)' :
@@ -70,6 +70,67 @@ const styles = {
 		border         : 'none',
 		textDecoration : 'none',
 		opacity        : '0',
+	},
+	boxContent      : {
+		position      : 'absolute',
+		padding       : '0.3rem',
+		width         : '100%',
+		left          : '0.0rem',
+		bottom        : '0.0rem',
+		color         : 'black',
+		letterSpacing : '0.1rem',
+		textTransform : 'uppercase',
+		fontSize      : '0.8rem',
+	},
+	copyOverlay     : {
+		opacity    : '0',
+		zIndex     : '0',
+		width      : '100%',
+		height     : '100%',
+		transition : 'transform 0.6s ease-in-out',
+		transform  : 'scale(0.1)',
+	},
+	showOverlay     : {
+		opacity   : '1',
+		transform : 'scale(50)',
+		zIndex    : '10',
+		position  : 'absolute',
+	},
+	copyMessage     : {
+		position       : 'fixed',
+		left           : '0',
+		right          : '0',
+		top            : '0',
+		bottom         : '0',
+		display        : 'flex',
+		flexDirection  : 'column',
+		alignItems     : 'center',
+		justifyContent : 'center',
+		fontSize       : '4rem',
+		transform      : 'scale(0.1)',
+		opacity        : '0',
+		color          : 'white',
+		'& h1'         : {
+			fontWeight    : '400',
+			textShadow    : '1px 2px black',
+			background    : 'rgba(255, 255, 255, 0.2)',
+			width         : '100%',
+			textAlign     : 'center',
+			marginBottom  : '0',
+			padding       : '1rem',
+			textTransform : 'uppercase',
+		},
+		'& p'          : {
+			fontSize   : '2rem',
+			fontWeight : '100',
+		},
+	},
+	showCopyMessage : {
+		opacity         : '1',
+		transform       : 'scale(1)',
+		zIndex          : '25',
+		transition      : 'all 0.4s ease-in-out',
+		transitionDelay : '0.3s',
 	},
 };
 
@@ -93,13 +154,16 @@ class ColorBox extends Component {
 		return (
 			<CopyToClipboard text={background} onCopy={this.changeCopyState}>
 				<div style={{ background: background }} className={classes.ColorBox}>
-					<div style={{ background: background }} className={`copy-overlay ${copied && 'show'}`} />
-					<div className={`copy-msg ${copied && 'show'}`}>
+					<div
+						style={{ background: background }}
+						className={`${classes.copyOverlay} ${copied && classes.showOverlay}`}
+					/>
+					<div className={`${classes.copyMessage} ${copied && classes.showCopyMessage}`}>
 						<h1>copied</h1>
 						<p className={classes.copyText}>{background}</p>
 					</div>
-					<div className='copy-container'>
-						<div className='box-content'>
+					<div>
+						<div className={classes.boxContent}>
 							<span className={classes.colorName}>{name}</span>
 						</div>
 						<button className={classes.copyButton}>Copy</button>
