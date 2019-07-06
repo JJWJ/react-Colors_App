c69fceb808d8a7060228d4e62717da589cec89b0
James J Williams JR <Jrotw@Jamess-MacBook-Pro.local>
2019-07-05 17:44:16 -0700

    Refactor CSS to JSS in Navbar removed css

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,8 @@ import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import 'rc-slider/assets/index.css';
 import Slider from 'rc-slider';
-import './Navbar.css';
+import { withStyles } from '@material-ui/styles';
+import styles from './styles/NavbarStyles';
 
 class Navbar extends Component {
 	constructor (props) {
@@ -28,22 +29,22 @@ class Navbar extends Component {
 		this.setState({ open: false });
 	}
 	render () {
-		const { level, changeLevel, isSingleColor } = this.props;
+		const { level, changeLevel, isSingleColor, classes } = this.props;
 		const { format, open } = this.state;
 		return (
-			<header className='Navbar'>
-				<div className='logo'>
+			<header className={classes.Navbar}>
+				<div className={classes.logo}>
 					<Link to='/'>Color Palette Creator</Link>
 				</div>
 				{!isSingleColor && (
-					<div className='slider-container'>
+					<div>
 						<span>Level: {level}</span>
-						<div className='slider'>
+						<div className={classes.slider}>
 							<Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={changeLevel} />
 						</div>
 					</div>
 				)}
-				<div className='select-container'>
+				<div className={classes.selectContainer}>
 					<Select value={format} onChange={this.handleFormatChange}>
 						<MenuItem value='hex'>HEX - #ffffff</MenuItem>
 						<MenuItem value='rgb'>RGB - rgb(255,255,255)</MenuItem>
@@ -70,4 +71,4 @@ class Navbar extends Component {
 	}
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
