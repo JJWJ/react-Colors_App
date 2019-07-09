9159dedac5f3c797081098416c7448cfebe5267d
James J Williams JR <Jrotw@Jamess-MacBook-Pro.local>
2019-07-08 18:56:24 -0700

    DraggableColorBox show boxes in NewPaletteForm

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -11,6 +11,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import Button from '@material-ui/core/Button';
+import DraggableColorBox from './DraggableColorBox';
 import { ChromePicker } from 'react-color';
 
 const drawerWidth = 400;
@@ -68,6 +69,7 @@ const styles = (theme) => ({
 	},
 	content      : {
 		flexGrow   : 1,
+		height     : 'calc(100vh - 64px)',
 		padding    : theme.spacing.unit * 3,
 		transition : theme.transitions.create('margin', {
 			easing   : theme.transitions.easing.sharp,
@@ -182,7 +184,7 @@ class NewPaletteForm extends Component {
 						[classes.contentShift]: open,
 					})}>
 					<div className={classes.drawerHeader} />
-					<ul>{colors.map((color) => <li style={{ backgroundColor: color }}>{color}</li>)}</ul>
+					{colors.map((color) => <DraggableColorBox color={color} />)}
 				</main>
 			</div>
 		);
