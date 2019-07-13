65da3cd436e2c10c57425645cec2d49a16218d7e
James J Williams JR <Jrotw@Jamess-MacBook-Pro.local>
2019-07-12 22:24:51 -0700

    ColorBox is now responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -1,4 +1,5 @@
 import chroma from 'chroma-js';
+import sizes from './sizes';
 
 export default {
 	ColorBox        : {
@@ -15,6 +16,27 @@ export default {
 		'&:hover $copyButton' : {
 			opacity    : 1,
 			transition : '0.5s',
+		},
+		[sizes.down('lg')]: {
+			width  : '25%',
+			height : (props) =>
+
+					props.showLink ? '20%' :
+					'50%',
+		},
+		[sizes.down('md')]: {
+			width  : '50%',
+			height : (props) =>
+
+					props.showLink ? '10%' :
+					'50%',
+		},
+		[sizes.down('xs')]: {
+			width  : '100%',
+			height : (props) =>
+
+					props.showLink ? '5%' :
+					'10%',
 		},
 	},
 	copyText        : {
