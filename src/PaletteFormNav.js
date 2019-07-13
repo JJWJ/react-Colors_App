7a5cb84ae5caf2223505a324bfdcdf05a48b6d91
James J Williams JR <Jrotw@Jamess-MacBook-Pro.local>
2019-07-12 19:54:14 -0700

    Styles improved in form

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -47,7 +47,7 @@ class PaletteFormNav extends Component {
 							color='inherit'
 							aria-label='Open drawer'
 							onClick={this.props.handleDrawerOpen}
-							className={classNames(classes.menuButton, open && classes.hide)}>
+							className={classNames(classes.menuButton, { [classes.hide]: open })}>
 							<MenuIcon />
 						</IconButton>
 						<Typography variant='h6' color='inherit' noWrap>
